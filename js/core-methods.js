5bce4743d4d990b167ec2111920eda15f70c76e1
wraith11 <wraith11@users.noreply.github.com>
2026-09-20 23:27:16 +0200

    Edit js/core-methods.js: replace '    _doSaveMap(name) {\n        // Aktuel' with '    _doSaveMap(name) {\n        // Aktuel'

--- js/core-methods.js
+++ js/core-methods.js
@@ -247,10 +247,8 @@ export const coreMethods = {
         this._doSaveMap(this.saveMapName);
     },
     _doSaveMap(name) {
-        // Aktuelle GM-View an den Backend-State melden, damit sie mitgespeichert wird.
-        // (view wird sonst nicht gebroadcastet, aber im Backend-State abgelegt.)
-        socket.emit('update_scene', { view: this.scene.view });
-        socket.emit('save_map', name, (res) => {
+        // Aktuelle GM-View direkt mit dem Speichern übertragen, damit sie mitgespeichert wird
+        socket.emit('save_map', { filename: name, view: this.scene.view }, (res) => {
             if(res.error) alert(this.t('errSave') + res.error);
             else {
                 this.currentMapName = name;
