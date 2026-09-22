822b4869418f0364e8a642ce1c3de9135cee01e7
wraith11 <wraith11@users.noreply.github.com>
2026-09-22 19:14:14 +0200

    Changed start scipts.

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Starting ScryTable...
-python lighttable.py %*
+python scrytable.py %*
 pause
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "Starting ScryTable..."
-python3 lighttable.py "$@"
+python3 scrytable.py "$@"
