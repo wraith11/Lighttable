93d18f92871199b5739d02ab70e37e5c5e7c2895
wraith11 <wraith11@users.noreply.github.com>
2026-09-25 17:27:14 +0200

    Edit js/i18n.js: replace '    camSetupBtn: "Camera Setup",\n    map' with '    camSetupBtn: "Camera Setup",\n    def'

--- js/i18n.js
+++ js/i18n.js
@@ -77,6 +77,7 @@ export const translations = {
     scale: "Scale:", remove: "Remove", repeat: "Repeat",
     // Settings tab
     camSetupBtn: "Camera Setup",
+    defaults: "Defaults", defTokenSize: "Default Token Size:", ringThickness: "Ring Thickness:",
     mapMgmt: "Map Management",
     mapName: "Map name", save: "Save", saveAs: "Save As", newMap: "New Map", confirmNewMap: "Create a new empty map? Unsaved changes will be lost.", deleteMap: "Delete map", confirmDeleteMap: "Delete map '%s'?", errDeleteMap: "Error deleting map: ",
     system: "System",
